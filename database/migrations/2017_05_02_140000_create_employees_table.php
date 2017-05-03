95657e8af969247a7f95794aac8a55877ab18717
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-03 11:54:03 +0100

    Create users (employees)

--- database/migrations/2017_05_02_140000_create_employees_table.php
+++ database/migrations/2017_05_02_140000_create_employees_table.php
@@ -4,7 +4,13 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+/**
+ * Create Employees Table
+ *
+ * @author EB
+ * Class CreateEmployeesTable
+ */
+class CreateEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +19,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('nickname');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
@@ -30,6 +37,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('employees');
     }
 }
