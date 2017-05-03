f05e2429e0ff52f9a86bfc962d239cca32876afd
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-03 11:54:09 +0100

    Create consumers

---
+++ database/migrations/2017_05_02_130000_create_consumers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Create Consumers Table
+ *
+ * @author EB
+ * @class CreateConsumersTable
+ */
+class CreateConsumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consumers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consumers');
+    }
+}
