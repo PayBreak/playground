bf23d712568b9021439c66649513647337d41a7f
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-04 09:08:27 +0100

    Front end skeleton

---
+++ resources/views/skeleton.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Css -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Stop vue kicking off (Cannot read property 'csrfToken' of undefined) -->
+        <script>
+            window.Laravel = <?php echo json_encode([
+                    'csrfToken' => csrf_token(),
+            ]); ?>
+        </script>
+    </head>
+    <body>
+        <div id="app">
+            @yield('content')
+        </div>
+
+        @yield('before-body-close')
+    </body>
+</html>
