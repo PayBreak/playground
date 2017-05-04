34ecb8b729f8f55485df39ebae44d3a66fbe84df
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-04 09:09:18 +0100

    Web routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+});
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', 'DashboardController@dashboard');
 });
