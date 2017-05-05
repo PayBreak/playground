ddf9c41367086e05987b78270e4c4e1e88c83194
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-05 14:21:35 +0100

    Tut said to add these scripts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,12 @@
 @extends('skeleton')
 
 @section('before-body-close')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.js" charset="utf-8"></script>
 <script src="{{ mix('/js/app.js') }}"></script>
+
 @endsection
 
 @section('content')
-<section class="hero">
-    <consumers></consumers>
-</section>
+    <app></app>
 @endsection
