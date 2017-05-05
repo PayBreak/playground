b1de33623f065198ff07ae7a2bdbf8778554d122
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-05 14:22:54 +0100

    App component

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="container">
+        <consumers></consumers>
+    </section>
+</template>
+
+<script>
+    import Consumers from './Consumers'
+
+    export default {
+        name: 'app',
+        components: {
+            consumers: Consumers
+        }
+    }
+</script>
