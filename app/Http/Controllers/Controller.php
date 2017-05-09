7ac59b6b39ae4d05d19d38d5d539cfbbc5c098af
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-09 16:51:17 +0100

    queryModel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Traits\FilterTrait;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, FilterTrait;
+
+    const DEFAULT_PAGINATION_COUNT = 15;
+
+    /**
+     * Default function for querying models
+     *
+     * @author EB
+     * @param Model $model
+     * @param Request $request
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function queryModel(Model $model, Request $request)
+    {
+        $query = $model->newQuery();
+        $filters = $request->only($query->getModel()->getVisible());
+
+        foreach ($filters as $k => $v) {
+            $query->where($k, 'like', '%' . $v . '%');
+        }
+
+        return $query->paginate($request->get('per_page', $this->getDefaultPaginationCount()));
+    }
+
+    /**
+     * Returns the pagination count
+     *
+     * @author EB
+     * @return int
+     */
+    protected function getDefaultPaginationCount()
+    {
+        return self::DEFAULT_PAGINATION_COUNT;
+    }
 }
