ef9808f47435614713101565e99483aa3429e17c
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-09 16:51:23 +0100

    Use queryModel

--- app/Http/Controllers/ConsumersController.php
+++ app/Http/Controllers/ConsumersController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Stack\Consumers;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 /**
@@ -19,25 +16,10 @@ class ConsumersController extends Controller
     /**
      * @author EB
      * @param Request $request
-     * @return Collection
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
     public function index(Request $request)
     {
-        /** @var Builder $query */
-        $query = Consumers::query();
-
-//        if ($request->has('first_name')) {
-//            $query->where('first_name', 'like', '%' . $request->get('first_name') . '%');
-//        }
-
-        $filters = $request->only($query->getModel()->getFillable());
-
-        foreach ($filters as $k => $v) {
-            $query->where($k, 'like', '%' . $v . '%');
-        }
-
-        $data = $query->paginate($request->get('per_page', 5));
-
-        return $data;
+        return $this->queryModel(new Consumers(), $request);
     }
 }
