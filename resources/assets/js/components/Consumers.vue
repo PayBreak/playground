a77e0effec62a555431414cd8c3ef58b0ed53c4e
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-09 15:26:51 +0100

    Messy components

--- resources/assets/js/components/Consumers.vue
+++ resources/assets/js/components/Consumers.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <filter-bar></filter-bar>
+        <filter-bar :filters="fields"></filter-bar>
         <vuetable ref="consumersTable"
             api-url="/api/consumers"
             :fields="fields"
@@ -22,6 +22,7 @@
 
 <script>
     import Vuetable from 'vuetable-2/src/components/Vuetable'
+    import VueEvents from 'vue-events'
 //    import VuetablePagination from 'vuetable-2/src/components/Vue tablePagination' // <-- Default
     import VuetablePagination from './layout/Pagination' // <-- Custom
     import VuetablePaginationInfo from './layout/PaginationInfo' // <-- Custom
@@ -31,6 +32,7 @@
 
     Vue.component('consumers-detail-row', DetailRow)   // <--- register the component to Vue
     Vue.component('consumers-filter-bar', FilterBar)   // <--- register the component to Vue
+    Vue.use(VueEvents)
 
     export default {
         components: {
@@ -45,6 +47,7 @@
                 fields: [
                     {
                         name: 'id',
+                        title: 'Id'
                     },
                     {
                         name: 'first_name',
@@ -54,17 +57,35 @@
                         name: 'last_name',
                         title: 'Last Name'
                     },
-                    'email',
+                    {
+                        name: 'email',
+                        title: 'Email Address'
+                    },
                     {
                         name: 'date_of_birth',
-                        title: 'DOB son',
+                        title: 'Date of Birth',
                         titleClass: 'sample-class-on-title-that-does-nothing'
                     }
                 ],
                 additionalQueryParameters: {
                     api_token: window.Laravel.api_token
-                }
+                },
+                defaultQueryParameters: {
+                    api_token: window.Laravel.api_token
+                },
+                addedFilters: {}
             }
+        },
+//        watch: {
+//            additionalQueryParameters () {
+//                this.remainingFilters = this.fields.filter(x => this.additionalQueryParameters.hasOwnProperty(x.name) > 0);
+//            }
+//        },
+        mounted () {
+//            this.$events.$on('filter-set', eventData => this.onFilterSet(eventData))
+            this.$events.$on('filter-set', eventData => this.onFilterAdd(eventData))
+            this.$events.$on('filter-remove', eventData => this.onFilterRemove(eventData))
+//            this.$events.$on('filter-reset', e => this.onFilterReset())
         },
         methods: {
             onPaginationData (paginationData) {
@@ -78,7 +99,33 @@
             onCellClicked (data, field, event) {
                 console.log('cellClicked: ', field.name)
                 this.$refs.consumersTable.toggleDetailRow(data.id)
+            },
+            onFilterAdd (eventData) {
+                let filters = {}
+                Object.keys(eventData).forEach(function(key) {
+                    console.log(eventData)
+                    filters[eventData[key].name] = eventData[key].filterValue
+                });
+
+                this.additionalQueryParameters = Object.assign(this.defaultQueryParameters, filters)
+                this.filterRefresh()
+            },
+            onFilterRemove (removedFilter) {
+                this.$delete(this.additionalQueryParameters, removedFilter)
+                this.filterRefresh()
+            },
+            filterRefresh() {
+                Vue.nextTick(() => this.$refs.consumersTable.refresh())
+            },
+            onFilterSet (filterText) {
+                this.additionalQueryParameters.first_name = filterText
+                Vue.nextTick( () => this.$refs.consumersTable.refresh())
+            },
+            onFilterReset () {
+                this.additionalQueryParameters = this.defaultQueryParameters
+                Vue.nextTick( () => this.$refs.consumersTable.refresh())
             }
         }
     }
 </script>
+diff = A.filter(x => B.indexOf(x) < 0 );
--- resources/assets/js/components/layout/Filter.vue
+++ resources/assets/js/components/layout/Filter.vue
@@ -1,13 +1,54 @@
+<!--<template>-->
+    <!--<div class="filter-bar ui basic segment grid">-->
+        <!--<div class="ui form">-->
+            <!--<div class="inline field">-->
+                <!--<label>Search for:</label>-->
+                <!--<input type="text" v-model="filterText" class="three wide column" @keyup.enter="doFilter" placeholder="name, nickname, or email">-->
+                <!--<button class="ui primary button" @click="doFilter">Go</button>-->
+                <!--<button class="ui button" @click="resetFilter">Reset</button>-->
+            <!--</div>-->
+        <!--</div>-->
+    <!--</div>-->
+<!--</template>-->
 <template>
-    <div class="filter-bar ui basic segment grid">
-        <div class="ui form">
-            <div class="inline field">
-                <label>Search for:</label>
-                <input type="text" v-model="filterText" class="three wide column" @keyup.enter="doFilter" placeholder="name, nickname, or email">
-                <button class="ui primary button" @click="doFilter">Go</button>
-                <button class="ui button" @click="resetFilter">Reset</button>
+    <div ref="filterTemplate">
+        <template v-if="filtersAdded">
+            <div class="content">
+                <template v-for="n, key in filtersAdded">
+                    <!--<div class="column">-->
+                        <!--<p>{{ n.title }}</p>-->
+                    <!--</div>-->
+                    <span class="tag is-primary">
+                        {{ n.title }}: {{n.filterValue}}
+                        <button class="delete is-small" @click="removeFilter(n.name, key)"></button>
+                    </span>
+                </template>
             </div>
-        </div>
+        </template>
+        <template v-if="filtersRemaining">
+            <div class="columns">
+                <div class="field">
+                    <p class="control">
+                        <span class="select">
+                            <select ref="filterSelect" v-model="filterName" required>
+                                <option hidden disabled selected value="">Filter option</option>
+                                <template v-for="n, key in filtersRemaining">
+                                    <option :value="n.name"
+                                            v-html="n.name"
+                                    ></option>
+                                </template>
+                            </select>
+                        </span>
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        <input class="input" type="text" v-model="filterValue" @keyup.enter="addFilter" ref="filterValue">
+                    </p>
+                </div>
+                <button type="button" @click="addFilter">Add filter</button>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -15,16 +56,60 @@
     export default {
         data () {
             return {
-                filterText: ''
+                filtersRemaining: {},
+                filtersAdded: {},
+                filterName: '',
+                filterValue: ''
             }
         },
-        methods: {
-            doFilter () {
-                console.log('doFilter:', this.filterText)
+        props: {
+            filters: {
+                type: Array,
+                default() {
+                    return []
+                }
             },
-            resetFilter () {
-                this.filterText = ''
-                console.log('resetFilter')
+        },
+        mounted () {
+            this.filtersRemaining = Array.from(this.filters)
+        },
+        methods: {
+            addFilter () {
+                let addIndex = this.filters.map((el) => el.name).indexOf(this.filterName)
+//                console.log(addIndex)
+                this.$set(this.filtersAdded, addIndex, this.filters[addIndex])
+                this.$set(this.filtersAdded[addIndex], 'filterValue', this.filterValue)
+
+                let removeIndex = this.filtersRemaining.map((el) => el.name).indexOf(this.filterName)
+                console.log(removeIndex)
+                this.$delete(this.filtersRemaining, removeIndex)
+
+//                this.$nextTick(function () {
+                    this.$events.fire('filter-set', this.filtersAdded)
+//                })
+//                this.filtersAdded[this.filterIndex] = this.filters[this.filterIndex]
+//                this.filtersAdded[this.filterIndex].filterValue = this.filterValue
+            },
+            removeFilter (filterName, key) {
+                console.log(filterName)
+                console.log(key)
+                let addIndex = this.filters.map((el) => el.name).indexOf(filterName)
+                this.$delete(this.filtersAdded, key)
+                this.$delete(this.filters[addIndex], 'filterValue')
+                this.filtersRemaining.push(this.filters[addIndex])
+                this.$events.fire('filter-remove', filterName)
+//                let addToRemainingIndex = this.filters.map((el) => el.name).indexOf(filterName)
+//                console.log(addToRemainingIndex)
+//                this.$delete(this.filtersAdded, key)
+//                console.log(this.filtersRemaining)
+//                console.log(this.filtersRemaining.length)
+////                this.$set(this.filtersRemaining, (this.filtersRemaining.length + 1), this.filters[addToRemainingIndex])
+//                console.log(this.filtersRemaining)
+//                this.$events.fire('filter-set', this.filtersAdded)
+//                console.log(filterName)
+            },
+            filterInputRefresh () {
+
             }
         }
     }
