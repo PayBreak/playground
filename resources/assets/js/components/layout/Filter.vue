f6e3a722010dd7726e37a1f711bac912d7b91bf7
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-05-09 16:51:38 +0100

    Some better styling

--- resources/assets/js/components/layout/Filter.vue
+++ resources/assets/js/components/layout/Filter.vue
@@ -11,44 +11,59 @@
     <!--</div>-->
 <!--</template>-->
 <template>
+    <div class="container">
     <div ref="filterTemplate">
         <template v-if="filtersAdded">
-            <div class="content">
-                <template v-for="n, key in filtersAdded">
-                    <!--<div class="column">-->
-                        <!--<p>{{ n.title }}</p>-->
-                    <!--</div>-->
-                    <span class="tag is-primary">
-                        {{ n.title }}: {{n.filterValue}}
-                        <button class="delete is-small" @click="removeFilter(n.name, key)"></button>
-                    </span>
-                </template>
+                <div class="level">
+                    <div class="level-left">
+                        <template v-for="n, key in filtersAdded">
+                            <div class="level-item">
+                                <span class="tag is-primary">
+                                {{ n.title }}: {{n.filterValue}}
+                                <button class="delete is-small" @click="removeFilter(n.name, key)"></button>
+                            </span>
+                            </div>
+                        </template>
+                    </div>
             </div>
         </template>
         <template v-if="filtersRemaining">
-            <div class="columns">
-                <div class="field">
-                    <p class="control">
-                        <span class="select">
-                            <select ref="filterSelect" v-model="filterName" required>
-                                <option hidden disabled selected value="">Filter option</option>
-                                <template v-for="n, key in filtersRemaining">
-                                    <option :value="n.name"
-                                            v-html="n.name"
-                                    ></option>
-                                </template>
-                            </select>
-                        </span>
-                    </p>
+            <div class="level">
+                <div class="level-left">
+                    <div class="level-item">
+                        <div class="field">
+                            <p class="control">
+                            <span class="select">
+                                <select ref="filterSelect" v-model="filterName" required>
+                                    <option hidden disabled selected value="">Filter option</option>
+                                    <template v-for="n, key in filtersRemaining">
+                                        <option :value="n.name"
+                                                v-html="n.name"
+                                        ></option>
+                                    </template>
+                                </select>
+                            </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="level-item">
+                        <div class="field">
+                            <p class="control">
+                                <input class="input" type="text" v-model="filterValue" @keyup.enter="addFilter" ref="filterValue">
+                            </p>
+                        </div>
+                    </div>
+                    <div class="level-item">
+                        <div class="field">
+                            <p class="control">
+                                <button class="button is-primary" type="button" @click="addFilter">Add filter</button>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div class="field">
-                    <p class="control">
-                        <input class="input" type="text" v-model="filterValue" @keyup.enter="addFilter" ref="filterValue">
-                    </p>
-                </div>
-                <button type="button" @click="addFilter">Add filter</button>
             </div>
         </template>
+    </div>
     </div>
 </template>
 
@@ -56,7 +71,7 @@
     export default {
         data () {
             return {
-                filtersRemaining: {},
+                filtersRemaining: [],
                 filtersAdded: {},
                 filterName: '',
                 filterValue: ''
@@ -75,38 +90,27 @@
         },
         methods: {
             addFilter () {
+                // Get the filter index which is passed into the component, from the selected filter in the drop down
                 let addIndex = this.filters.map((el) => el.name).indexOf(this.filterName)
-//                console.log(addIndex)
                 this.$set(this.filtersAdded, addIndex, this.filters[addIndex])
                 this.$set(this.filtersAdded[addIndex], 'filterValue', this.filterValue)
 
                 let removeIndex = this.filtersRemaining.map((el) => el.name).indexOf(this.filterName)
-                console.log(removeIndex)
                 this.$delete(this.filtersRemaining, removeIndex)
 
-//                this.$nextTick(function () {
-                    this.$events.fire('filter-set', this.filtersAdded)
-//                })
-//                this.filtersAdded[this.filterIndex] = this.filters[this.filterIndex]
-//                this.filtersAdded[this.filterIndex].filterValue = this.filterValue
+                this.$events.fire('filter-set', this.filtersAdded)
             },
             removeFilter (filterName, key) {
-                console.log(filterName)
-                console.log(key)
-                let addIndex = this.filters.map((el) => el.name).indexOf(filterName)
+                // Get the index of the filter which was passed into the component originally
+                let givenFilterIndex = this.filters.map((el) => el.name).indexOf(filterName)
+                // Remove, from the list of currently added filters, the filter which is being removed
                 this.$delete(this.filtersAdded, key)
-                this.$delete(this.filters[addIndex], 'filterValue')
-                this.filtersRemaining.push(this.filters[addIndex])
+                // Remove the 'filterValue' on the original given filter index (Due to JavaScript referencing)
+                this.$delete(this.filters[givenFilterIndex], 'filterValue')
+                // Populate remaining filters list again
+                this.filtersRemaining.push(this.filters[givenFilterIndex])
+                // Fire event so other components can handle changes
                 this.$events.fire('filter-remove', filterName)
-//                let addToRemainingIndex = this.filters.map((el) => el.name).indexOf(filterName)
-//                console.log(addToRemainingIndex)
-//                this.$delete(this.filtersAdded, key)
-//                console.log(this.filtersRemaining)
-//                console.log(this.filtersRemaining.length)
-////                this.$set(this.filtersRemaining, (this.filtersRemaining.length + 1), this.filters[addToRemainingIndex])
-//                console.log(this.filtersRemaining)
-//                this.$events.fire('filter-set', this.filtersAdded)
-//                console.log(filterName)
             },
             filterInputRefresh () {
 
