659c81eb7d083c9e627fb173d1623de46bc412f3
Simon Dawson <simon.dawson@paybreak.com>
2017-08-17 16:25:04 +0100

    Baseline

--- vuetable-l54/resources/assets/js/app.js
+++ vuetable-l54/resources/assets/js/app.js
@@ -15,8 +15,91 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('my-vuetable', require('./components/MyVuetable.vue'));
+import Vuetable from 'vuetable-2/src/components/Vuetable';
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
+import Vue from 'vue';
+import VueEvents from 'vue-events';
+Vue.use(VueEvents)
 
-const app = new Vue({
-    el: '#app'
-});
+const app =new Vue({
+    el: '#app',
+    components: {
+        Vuetable,
+        VuetablePagination,
+        VuetablePaginationInfo,
+    },
+    data: {
+        fields: [
+            {
+                name: 'name',
+                title: '<span class="glyphicon glyphicon-user"></span> Full Name',
+                sortField: 'name'
+            },
+            {
+                name: 'email',
+                title: 'Email',
+                sortField: 'email'
+            },
+            'birthdate',
+            'nickname',
+            {
+                name: 'gender',
+                title: 'Gender',
+                sortField: 'gender'
+            },
+            '__slot:actions'
+        ],
+        sortOrder: [
+            { field: 'name', direction: 'asc' }
+        ],
+        css: {
+            table: {
+                tableClass: 'table table-striped table-bordered table-hovered',
+                loadingClass: 'loading',
+                ascendingIcon: 'glyphicon glyphicon-chevron-up',
+                descendingIcon: 'glyphicon glyphicon-chevron-down',
+                handleIcon: 'glyphicon glyphicon-menu-hamburger',
+            },
+            pagination: {
+                infoClass: 'pull-left',
+                wrapperClass: 'vuetable-pagination pull-right',
+                activeClass: 'btn-primary',
+                disabledClass: 'disabled',
+                pageClass: 'btn btn-border',
+                linkClass: 'btn btn-border',
+                icons: {
+                    first: '',
+                    prev: '',
+                    next: '',
+                    last: '',
+                },
+            }
+        }
+    },
+    computed:{
+        /*httpOptions(){
+          return {headers: {'Authorization': "my-token"}} //table props -> :http-options="httpOptions"
+        },*/
+    },
+    methods: {
+        onPaginationData (paginationData) {
+            this.$refs.pagination.setPaginationData(paginationData)
+        },
+        onChangePage (page) {
+            this.$refs.vuetable.changePage(page)
+        },
+        editRow(rowData){
+            alert("You clicked edit on"+ JSON.stringify(rowData))
+        },
+        deleteRow(rowData){
+            alert("You clicked delete on"+ JSON.stringify(rowData))
+        },
+        onLoading() {
+            console.log('loading... show your spinner here')
+        },
+        onLoaded() {
+            console.log('loaded! .. hide your spinner here')
+        }
+    }
+})
--- vuetable-l54/resources/views/home.blade.php
+++ vuetable-l54/resources/views/home.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <my-vuetable></my-vuetable>
+    <div id="app">
+        <div id="table-wrapper" class="ui container">
+            <h2><strong>&lt;Vuetable-2&gt;</strong> with Bootstrap 3</h2>
+            <div id="spinner" class="loading" style="display:none">Loading&#8230;</div>
+            <vuetable ref="vuetable"
+                      api-url="https://vuetable.ratiw.net/api/users"
+                      :fields="fields"
+                      :sort-order="sortOrder"
+                      :css="css.table"
+                      pagination-path=""
+                      :per-page="25"
+                      @vuetable:pagination-data="onPaginationData"
+                      @vuetable:loading="onLoading"
+                      @vuetable:loaded="onLoaded"
+            >
+                <template slot="actions" scope="props">
+                    <div class="table-button-container">
+                        <button class="btn btn-warning btn-sm" @click="editRow(props.rowData)">
+                            <span class="glyphicon glyphicon-pencil"></span> Edit</button>&nbsp;&nbsp;
+                        <button class="btn btn-danger btn-sm" @click="deleteRow(props.rowData)">
+                            <span class="glyphicon glyphicon-trash"></span> Delete</button>&nbsp;&nbsp;
+                    </div>
+                </template>
+            </vuetable>
+            <vuetable-pagination ref="pagination"
+                                 :css="css.pagination"
+                                 @vuetable-pagination:change-page="onChangePage"
+            ></vuetable-pagination>
+        </div>
+
     </div>
 @endsection
