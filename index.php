35951aaa04d062a1825cbb033dc02759d210d891
Gergely Kreko <gregory.kreko@paybreak.com>
2018-06-29 09:49:34 +0100

    Fix scripts, ensured they are functioning as supposed to

--- index.php
+++ index.php
@@ -12,29 +12,25 @@
                 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
                 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
             })(window,document.documentElement,'async-hide','dataLayer',4000,
-                {'GTM-TFFRRV6':true});</script>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-121562509-1', 'auto');
-            ga('require', 'GTM-TFFRRV6');
-            ga('send', 'pageview');
+                {'GTM-M63T3NR':true});
         </script>
-        <title>Greg Test</title>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121562509-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M63T3NR');</script>
+        <!-- End Google Tag Manager -->
 
-            gtag('config', 'UA-121562509-1');
-        </script>
+
+        <title>Greg Test</title>
     </head>
 
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M63T3NR"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <h1>Greg Test</h1>
         <h2>send me stuff</h2>
         <form>
