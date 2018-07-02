8850107f57f440dc31bdbbcf11dc6977eb73e439
Gergely Kreko <gregory.kreko@paybreak.com>
2018-07-02 10:26:48 +0100

    Serverside ab

--- index.php
+++ index.php
@@ -6,6 +6,52 @@
      **/
 
 require_once('./vendor/autoload.php');
+
+$ttl = 86400; // in seconds
+$adapter = new \PhpAb\Storage\Adapter\Cookie('phpab', $ttl);
+$storage = new \PhpAb\Storage\Storage($adapter);
+$participationManager = new \PhpAb\Participation\Manager($storage);
+
+$analyticsData = new \PhpAb\Analytics\DataCollector\Google();
+
+$dispatcher = new \PhpAb\Event\Dispatcher();
+$dispatcher->addSubscriber($analyticsData);
+
+$filter = new \PhpAb\Participation\Filter\Percentage(50);
+
+$chooser = new \PhpAb\Variant\Chooser\RandomChooser();
+
+$engine = new \PhpAb\Engine\Engine($participationManager, $dispatcher, $filter, $chooser);
+
+$test = new \PhpAb\Test\Test(
+    'First Experiment',
+    [
+        new PhpAb\Variant\SimpleVariant('Control'),
+        new PhpAb\Variant\CallbackVariant('Different layout 1', function () {
+            die('This is the alternative variant speaking. YEABOIIII');
+            // push stuff into seassions?
+        }),
+    ],
+    [
+        \PhpAb\Analytics\DataCollector\Google::EXPERIMENT_ID => 'vstptpKtRaSQNPSlbeQpBw'
+    ]
+);
+
+
+try {
+    $engine->addTest($test);
+    $engine->start();
+    $engine->getTest('First Experiment');
+} catch (\Exception $e) {
+    echo '<h1>The engine went bust:</h1>';
+    var_dump($e->getMessage());
+    die();
+}
+
+$dispatcher->dispatch('Page visited', []);
+
+$analytics = new \PhpAb\Analytics\Renderer\Google\GoogleUniversalAnalytics($analyticsData->getTestsData());
+
 ?>
 <html>
     <head>
@@ -33,6 +79,7 @@ require_once('./vendor/autoload.php');
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M63T3NR"
                           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
+        <pre><?php echo $analytics->getScript(); ?></pre>
         <h1>Greg Test</h1>
         <h2>send me stuff</h2>
         <form>
